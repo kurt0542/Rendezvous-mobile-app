e3d4c1921b633d841722fa160cbc68207f228321
kurt <rond9275@gmail.com>
2025-08-11 22:05:40 +0800

    added distance calculation with nearby search service

--- lib/services/nearby_search_service.dart
+++ lib/services/nearby_search_service.dart
@@ -1,5 +1,52 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:location/location.dart';
+import 'package:mobile_app_project/utils/location_service.dart';
 class NearbySearchService {
+  final String? apiKey = dotenv.env['GOOGLE_PLACES_API_KEY'];
 
+  Future<List<dynamic>> getPosts() async {
+    final locationService = LocationService();
+    final locationData = await locationService.getLongLatLocation();
+    if (locationData == null) {
+      throw Exception("Location not available");
+    }
+    final latitude = locationData["latitude"]!;
+    final longitude = locationData["longitude"]!;
+
+    var uri = Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+            '?location=$latitude,$longitude'
+            '&keyword=coffee'
+            '&type=cafe'
+            '&rankby=distance'
+            '&key=$apiKey');
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['results'];
+    } else {
+      throw Exception("Failed to fetch nearby places");
+    }
+  }
+
+  double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    const earthRadius = 6371;
+    double _degreesToRadian(double degrees) {
+      return degrees * pi / 180;
+    }
+    final differenceInLatitude = _degreesToRadian(lat2 - lat1);
+    final differenceInLongitude = _degreesToRadian(lon2 - lon1);
+
+    final a = sin(differenceInLatitude / 2) * sin(differenceInLatitude / 2) +
+        cos(_degreesToRadian(lat1)) *
+            cos(_degreesToRadian(lat2)) *
+            sin(differenceInLongitude / 2) *
+            sin(differenceInLongitude / 2);
+
+    final c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+    return earthRadius * c;
+  }
 }
--- lib/utils/location_service.dart
+++ lib/utils/location_service.dart
@@ -1,26 +1,26 @@
 import 'package:location/location.dart';
 
 class LocationService {
-  Future<Map<String,String>?> GetLongLatLocation() async {
-    Location location = Location();
+  Location location = Location();
+  Future<Map<String, String>?> getLongLatLocation() async {
 
 
     bool serviceEnabled = await location.serviceEnabled();
-    if(!serviceEnabled){
+    if (!serviceEnabled) {
       serviceEnabled = await location.requestService();
-      if(!serviceEnabled) return null;
+      if (!serviceEnabled) return null;
     }
 
     PermissionStatus permissionGranted = await location.hasPermission();
-    if(permissionGranted == PermissionStatus.denied){
+    if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await location.requestPermission();
-      if(permissionGranted == PermissionStatus.granted) return null;
+      if (permissionGranted != PermissionStatus.granted) return null;
     }
 
     LocationData currentLocation = await location.getLocation();
     return {
-      "latitude" : currentLocation.latitude?.toString() ?? "",
-      "longitude" : currentLocation.longitude?.toString() ?? ""
+      "latitude": currentLocation.latitude?.toString() ?? "",
+      "longitude": currentLocation.longitude?.toString() ?? ""
     };
   }
 }
